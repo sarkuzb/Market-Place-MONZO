5d192a1f61132dd07cbd9524e9a7c6e93e98254e
sarkuzb <ssmartprinter@gmail.com>
2024-11-07 17:33:03 +0500

    first commit

---
+++ src/category.js
@@ -0,0 +1,42 @@
+export const Category = [
+  {
+    id: 1,
+    category: "Monitors",
+  },
+  {
+    id: 2,
+    category: "Notebooks",
+  },
+  {
+    id: 3,
+    category: "Keyboards",
+  },
+  {
+    id: 4,
+    category: "Mouses",
+  },
+  {
+    id: 5,
+    category: "Mousepads",
+  },
+  {
+    id: 6,
+    category: "Headphones",
+  },
+  {
+    id: 7,
+    category: "Microphones",
+  },
+  {
+    id: 8,
+    category: "RAMs",
+  },
+  {
+    id: 9,
+    category: "Processors",
+  },
+  {
+    id: 10,
+    category: "Graphic Cards",
+  },
+];
