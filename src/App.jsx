291f54dd8e5cd797fca239165e9deff838073984
sarkuzb <ssmartprinter@gmail.com>
2024-11-07 17:52:39 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./home.jsx";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter basename="/Market-Place-MONZO/">
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
+      {" "}
+      {/* Set the basename here */}
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
